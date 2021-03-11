f9eafdd2da104718acc41dbf8c1e2afd6a7455b9
Carolina M <carolinamargiott2i@gmail.com>
2021-03-11 08:21:54 -0300

    adicionando novo spring boot para novos requisitos

---
+++ projetopoo/src/test/java/com/jerso/projetopoo/ProjetopooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerso.projetopoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
