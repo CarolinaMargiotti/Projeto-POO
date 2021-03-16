0b031832121f47215234f4c1d5a25b57944895cf
nicolastelles <nico-telles@hotmail.com>
2021-03-16 10:53:30 -0300

    add and remove functions

--- projetopoo/src/main/java/com/jerso/projetopoo/model/unidade/Unidades.java
+++ projetopoo/src/main/java/com/jerso/projetopoo/model/unidade/Unidades.java
@@ -10,6 +10,9 @@ import javax.persistence.Entity;
 public class Unidades {
     private List<Unidade> _listaUnidades;
 
+    
+    //teste de commit 
+    
             public void AddUnidades(Unidade u){
         _listaUnidades.add(u);
     }
