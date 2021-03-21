6df0f0e5ff74a225bb9900ce23cdd839ca21410b
Carolina M <carolinamargiott2i@gmail.com>
2021-03-21 18:29:30 -0300

    Editar unidade (wip)

--- projetopoo/src/main/java/com/jerso/projetopoo/controller/UnidadesController.java
+++ projetopoo/src/main/java/com/jerso/projetopoo/controller/UnidadesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
@@ -38,6 +39,20 @@ public class UnidadesController {
         u.setCep(cep);
         ur.save(u);
         return "cadastrarUnidade";
+    }
+
+    @GetMapping("/editar-unidade/{id}")
+    public String edit(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("editarUnidade");
+        Unidade u = ur.findById(id).get();
+        mv.addObject("unidade", u);
+        return "editarUnidade";
+    }
+
+    @PostMapping("/editar-unidade/{id}")
+    public String edit(@PathVariable("id") long id, Unidade u) {
+        ur.save(u);
+        return "listaUnidades";
     }
 
 }
